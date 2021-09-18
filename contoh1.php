16ac22aa10f834f29899e8d1acf948252d5d9074
dimasalis <dimasalisadewo911@gmail.com>
2021-09-18 11:08:05 +0700

    penambahan isi contoh1.php

---
+++ contoh1.php
@@ -0,0 +1,11 @@
+<?php
+class Contoh1 extends CI_Controller
+{
+    public function index()
+    {
+        echo "<h1>Perkenalkan</h1>";
+        echo "Nama Saya Dimas Ali Sadewo";
+        echo "Saya tinggal di Pulogadung Jakarta Timur"; 
+        echo "Saya Mahasiswa UBSI PEMUDA jurusan Sistem Informasi";
+    }
+}
