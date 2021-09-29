da46d61c45546f178d19b6fc269b050ab33786b8
dimasalis <dimasalisadewo911@gmail.com>
2021-09-30 00:15:12 +0700

    Upload tugas

---
+++ view-latihan1.php
@@ -0,0 +1,23 @@
+<html>
+    
+<head>
+    <title>Latihan 1</title>
+</head>
+
+<body>
+    Halo Kawan.. Yuk kita belajar web programming..!!!<br>
+    Nil 1 = <?= $nilai1; ?>
+    Nil 2 = <?= $nilai2; ?>
+    ini hasil dari pemodelan dengan methode penjumlahan yaitu <?=
+$nilai1 . " + " . $nilai2 . " = " . $hasil; ?>
+
+</body>
+
+</html>
+
+
+
+
+
+
+
